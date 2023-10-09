ab558dfc60f967ec9d8b5bcfe3f31bda823e4cb0
cko003 <cko003@bucknell.edu>
2023-10-09 18:23:36 -0400

    issue with robots.txt blocking

--- igbo/igbo/spiders/myigbonamespider.py
+++ igbo/igbo/spiders/myigbonamespider.py
@@ -8,32 +8,30 @@ class MyigbonamespiderSpider(scrapy.Spider):
 
     def parse(self, response):
         names = response.css('.flexi .item .content')
-        
+
         for name in names:
             start_with_url = name.css('a::attr(href)').get()
-            
+
             if start_with_url is not None:
                 letter_url = 'https://www.myigboname.com' + start_with_url
-                yield response.follow(letter_url, callback = self.parse)
-            
+                yield response.follow(letter_url, callback=self.parse)
+
             begin_with_letter = response.css('.ui .item .content a')
-            for letter in begin_with_letter:
-                name_url = letter.css('a::attr(href)').get()
-                
+            for name in begin_with_letter:
+                name_url = name.css('a::attr(href)').get()
+
                 if name_url is not None:
-                    yield response.follow(name_url, callback = self.parse_name_page)
-                    
-                    
+                    yield response.follow(name_url, callback=self.parse_name_page)
+
     def parse_letter_page(self, response):
         pass
-                    
+
     def parse_name_page(self, response):
-        
-        yield{
+
+        yield {
             "name": response.css('.very h1::text').get(),
-            "transcription" : response.xpath('/html/body/div[2]/div/div/div[2]/div/p[1]/text()').get(),
-            "language" : "Igbo",
+            "transcription": response.xpath('/html/body/div[2]/div/div/div[2]/div/p[1]/text()').get(),
+            "language": "Igbo",
             "gender": response.xpath('/html/body/div[2]/div/div/div[2]/div/p[2]/strong/text()').get().strip(),
             'meaning': response.xpath('/html/body/div[2]/div/div/div[2]/div/p[3]/strong/text()').get().strip(),
         }
-    
