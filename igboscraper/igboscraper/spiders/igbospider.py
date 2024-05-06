07a9833431b7f2ef760c4cad84e8a6431efbe28a
cko003 <cko003@bucknell.edu>
2024-05-06 23:17:56 +0100

    scrape igbo names and export to csv

--- igboscraper/igboscraper/spiders/igbospider.py
+++ igboscraper/igboscraper/spiders/igbospider.py
@@ -1,16 +1,51 @@
+from typing import Iterable
 import scrapy
 
 
 class IgbospiderSpider(scrapy.Spider):
     name = 'igbospider'
     allowed_domains = ['myigboname.com']
-    start_urls = ['https://www.myigboname.com/start-with/a']
+    start_urls = ['https://www.myigboname.com/start-with/a',
+                  'https://www.myigboname.com/start-with/b',
+                  'https://www.myigboname.com/start-with/c',
+                  'https://www.myigboname.com/start-with/d',
+                  'https://www.myigboname.com/start-with/e',
+                  'https://www.myigboname.com/start-with/f',
+                  'https://www.myigboname.com/start-with/g',
+                  'https://www.myigboname.com/start-with/i',
+                  'https://www.myigboname.com/start-with/j',
+                  'https://www.myigboname.com/start-with/k',
+                  'https://www.myigboname.com/start-with/l',
+                  'https://www.myigboname.com/start-with/m',
+                  'https://www.myigboname.com/start-with/n',
+                  'https://www.myigboname.com/start-with/o',
+                  'https://www.myigboname.com/start-with/r',
+                  'https://www.myigboname.com/start-with/s',
+                  'https://www.myigboname.com/start-with/t',
+                  'https://www.myigboname.com/start-with/u',
+                  'https://www.myigboname.com/start-with/w',
+                  'https://www.myigboname.com/start-with/z',]
 
     def parse(self, response):
         names = response.css('.content a')
         
         for name in names:
-            yield{
-                'name' : name.css(' a::text').get().strip(),
-                'url' : 'https://www.myigboname.com{0}'.format(name.css('a::attr(href)').get())
-            }
+            # yield{
+            #     'name' : name.css(' a::text').get().strip(),
+            #     'url' : 'https://www.myigboname.com{0}'.format(name.css('a::attr(href)').get())
+            # }
+            
+            meaning_page_url =  'https://www.myigboname.com{0}'.format(name.css('a::attr(href)').get())
+            # yield response.follow(meaning_page_url, callback=self.parseMeaning)
+            yield scrapy.Request(meaning_page_url, callback=self.parseMeaning)
+
+    def parseMeaning(self, response):
+        
+        yield{
+            'name' : response.css('h1::text').get().strip(),
+            'transcription': response.css('.very p::text').get(default='-').strip(),
+            'meaning' : response.css('div.very.padded h1 + p + p + p strong::text').get(default='unknown').strip(),
+            'gender': response.css('.very:last-child strong::text').get(default='-'),
+            'language': 'igbo'
+            
+        }
